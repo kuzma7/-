27bbdecf63019559175657aa3926880343b0fa8e
Nikita Kuzminov <clashyama@yandex.ru>
2024-11-11 16:21:54 +0300

    First commit

---
+++ main.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+df = pd.read_excel('Нагрузка для чтения.csv', engine='openpyxl', header=[0, 1])
+
+
+print(df.head())
+
+json_data = df.to_json(orient='records', force_ascii=False)
+
+with open('output.json', 'w', encoding='utf-8') as f:
+    f.write(json_data)
